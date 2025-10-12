197f012535e16e871f969156df85ce72976f24dc
BrokenK3yboard <63761658+BrokenK3yboard@users.noreply.github.com>
2025-10-12 16:11:00 -0500

    Add config option to hide Jade overlay

---
+++ fabric/src/main/java/com/brokenkeyboard/usefulspyglass/mixin/WailaTickHandlerMixin.java
@@ -0,0 +1,25 @@
+package com.brokenkeyboard.usefulspyglass.mixin;
+
+import com.brokenkeyboard.usefulspyglass.config.CommonConfig;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import snownee.jade.api.config.IWailaConfig;
+import snownee.jade.overlay.WailaTickHandler;
+
+@Pseudo
+@Mixin(WailaTickHandler.class)
+public class WailaTickHandlerMixin {
+
+    @WrapOperation(method = "tickClient", at = @At(value = "INVOKE", target = "Lsnownee/jade/api/config/IWailaConfig$IConfigGeneral;shouldDisplayTooltip()Z"), require = 0, remap = false)
+    private boolean shouldDisplayToolttip(IWailaConfig.IConfigGeneral instance, Operation<Boolean> original) {
+        if (CommonConfig.JADE_REQUIRES_SPYGLASS.get() && Minecraft.getInstance().player != null) {
+            return original.call(instance) && Minecraft.getInstance().player.isScoping();
+        } else {
+            return original.call(instance);
+        }
+    }
+}
